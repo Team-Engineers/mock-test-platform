9cdb5491b4ad186bf53de8aff5082f2a77fb5a5f
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2024-03-02 02:44:54 +0530

    made public 1 mocktest

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router'
+import React from "react";
+import { Navigate, Outlet } from "react-router";
+import { DEFAULTUSER } from "./constants";
+import { useParams } from "react-router-dom";
 
 const PrivateRoutes = () => {
+  const { id } = useParams();
+  if (id === "free") {
+    localStorage.setItem("user", JSON.stringify(DEFAULTUSER));
+  }
   const isUserSignedIn = () => {
-    const tokenData = JSON.parse(localStorage.getItem("accessToken"));
-    return tokenData && new Date().getTime() < tokenData.expiry;
+    const userData = JSON.parse(localStorage.getItem("user"));
+    return userData?.name;
   };
-  return (
-    isUserSignedIn() ? <Outlet/> : <Navigate to = "/login"/>
-  )
-}
+  return isUserSignedIn() ? <Outlet /> : <Navigate to="/login" />;
+};
 
-export default PrivateRoutes
+// {"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NTNjMDJmNjdiYzE0MTM2NWE2NGE2YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwODk2Mzk1OSwiZXhwIjoxNzA5Mzk1OTU5fQ.vd_2XfE9tzwlh_R5deaWuwyeyGZfK1idFjt1g_R62W4","expiry":1709395960292}
+export default PrivateRoutes;
